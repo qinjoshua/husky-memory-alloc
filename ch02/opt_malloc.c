a9f9c9b136d79c469fd2f43c36c9fcc5999985c3
sam-zlota <aG~%f7{Bks`57XDNE-iXU>
2021-03-30 21:51:59 -0400

    asert

--- ch02/opt_malloc.c
+++ ch02/opt_malloc.c
@@ -240,8 +240,10 @@ void xfree(void* ptr) {
   uint64_t address = (uint64_t)ptr;
   void* pageStart = (void*) (address - (address % PAGE_SIZE));
 
+  assert(*(long*)pageStart != 0);
   // If we cast it to a long, does it have that magic number?
   while (*(long*)pageStart != MAGIC_NUMBER) {
+      assert(*(long*)pageStart != 0);
     pageStart -= PAGE_SIZE;
   }
 
@@ -324,8 +326,11 @@ void* xrealloc(void* prev, size_t bytes) {
   uint64_t address = (uint64_t)prev;
   void* pageStart = (void*) (address - (address % PAGE_SIZE));
 
+  assert(*(long*)pageStart != 0);
+
   // If we cast it to a long, does it have that magic number?
   while (*(long*)pageStart != MAGIC_NUMBER) {
+      assert(*(long*)pageStart != 0);
     pageStart -= PAGE_SIZE;
   }
 
